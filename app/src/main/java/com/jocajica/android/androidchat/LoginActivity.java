0f5c1fec4cbfbbf06b65c2cfa9dde99cf0ac123e
jcarles <jcarles@kitchenmaster.es>
2016-06-08 08:46:54 +0200

    Afegida la llibreria ButterKnife

--- app/src/main/java/com/jocajica/android/androidchat/LoginActivity.java
+++ app/src/main/java/com/jocajica/android/androidchat/LoginActivity.java
@@ -2,12 +2,45 @@ package com.jocajica.android.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.btnSignin)
+    Button btnSigning;
+    @BindView(R.id.btnSignup)
+    Button btnSignup;
+    @BindView(R.id.editTxtEmail)
+    EditText inputEmail;
+    @BindView(R.id.editTxtPassword)
+    EditText inputPassword;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.layoutMainContainer)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSigning() {
+
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleSignup() {
+
+    }
+
 }
+
