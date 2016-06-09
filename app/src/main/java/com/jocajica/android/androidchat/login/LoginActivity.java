61c6be19de5f053e01dc01d1efe60b4c861112ba
jcarles <jcarles@kitchenmaster.es>
2016-06-09 08:30:15 +0200

    Implementació de la interficie LoginView

--- app/src/main/java/com/jocajica/android/androidchat/login/LoginActivity.java
+++ app/src/main/java/com/jocajica/android/androidchat/login/LoginActivity.java
@@ -1,19 +1,23 @@
 package com.jocajica.android.androidchat.login;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.jocajica.android.androidchat.R;
+import com.jocajica.android.androidchat.contactlist.ContactListActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
     @BindView(R.id.btnSignin)
     Button btnSigning;
     @BindView(R.id.btnSignup)
@@ -26,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     ProgressBar progressBar;
     @BindView(R.id.layoutMainContainer)
     RelativeLayout container;
+
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +50,66 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btnSignup)
+    @Override
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(), inputPassword.getText().toString());
+    }
+
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(), inputPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin, error));
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup, error));
+        inputPassword.setError(msgError);
+    }
+
+    private void setInputs(boolean enabled) {
+        inputEmail.setEnabled(enabled);
+        inputPassword.setEnabled(enabled);
+        btnSigning.setEnabled(enabled);
+        btnSignup.setEnabled(enabled);
+    }
 }
 
