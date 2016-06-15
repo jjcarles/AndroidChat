58f4c977d1950d695379e16b1e7db9d256b1c15d
jjcarles <jjcarles@jocajica.com>
2016-06-15 12:32:16 +0200

    Implementació de l'Interactor.

---
+++ app/src/main/java/com/jocajica/android/androidchat/contactlist/ContactListRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.jocajica.android.androidchat.contactlist;
+
+/**
+ * Created by Falken on 15/06/2016.
+ */
+public class ContactListRepositoryImpl implements ContactListRepository {
+    @Override
+    public void signOff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void subscribeToContactListEvents() {
+
+    }
+
+    @Override
+    public void unsubscribeToContactListEvents() {
+
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
